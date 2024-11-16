7611efb5ffc4cbf9c03082937e796ef2dea7ab9d
Radostin <Radostin@DESKTOP-5G5P7FJ>
2024-11-16 13:23:23 +0200

    adminaddusers.cs started

--- AdminAddUsers.cs
+++ AdminAddUsers.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Invento
 {
     public partial class AdminAddUsers : Form
     {
+        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Radostin\Documents\invento.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=False");
         public AdminAddUsers()
         {
             InitializeComponent();
@@ -19,7 +22,76 @@ namespace Invento
 
         private void addUsers_addBtn_Click(object sender, EventArgs e)
         {
+            if (addUsers_username.Text == "" || addUsers_password.Text == "" || addUsers_role.SelectedIndex == -1
+                || addUsers_status.SelectedIndex == -1)
+            {
+                MessageBox.Show("Empty Fields", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (checkConnection())
+                {
+                    try
+                    {
+                        connect.Open();
 
+                        string checkUsername = "SELECT * FROM user WHERE username = @usern";
+
+                        using (SqlCommand cmd = new SqlCommand(checkUsername, connect))
+                        {
+                            cmd.Parameters.AddWithValue("@usern", addUsers_username.Text.Trim());
+
+                            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                            DataTable table = new DataTable();
+                            adapter.Fill(table);
+
+                            if (table.Rows.Count > 0)
+                            {
+                                MessageBox.Show(addUsers_username.Text.Trim() 
+                                    + "is already used", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                            else
+                            {
+                                string insertData = "INSERT INTO users (username, password, role, status, date) VALUES(@usern, @pass, @role, @status, @date)";
+                                using (SqlCommand insertD = new SqlCommand(insertData, connect)) 
+                                {
+                                    insertD.Parameters.AddWithValue("@usern", addUsers_username.Text.Trim());
+                                    insertD.Parameters.AddWithValue("@pass", addUsers_password.Text.Trim());
+                                    insertD.Parameters.AddWithValue("@role", addUsers_role.SelectedItem.ToString());
+                                    insertD.Parameters.AddWithValue("@status", addUsers_status.SelectedItem.ToString());
+
+                                    DateTime today = DateTime.Today;
+                                    insertD.Parameters.AddWithValue("@date", today);
+
+                                    insertD.ExecuteNonQuery();
+                                    
+                                    MessageBox.Show("Added Succesfully", "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Connection failed: " + ex, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally 
+                    {
+                        connect.Close();
+                    }
+                }
+            }  
+        }
+
+        public bool checkConnection()
+        {
+            if (connect.State == ConnectionState.Closed)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
