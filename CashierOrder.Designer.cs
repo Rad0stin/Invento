9198713093b1cd1da95041880f9e666b47f95482
Radostin <Radostin@DESKTOP-5G5P7FJ>
2025-01-03 23:40:39 +0200

    102, receipt started only graphic

--- CashierOrder.Designer.cs
+++ CashierOrder.Designer.cs
@@ -28,10 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CashierOrder));
             this.panel1 = new System.Windows.Forms.Panel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
@@ -62,6 +63,8 @@
             this.label10 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
             this.cashierOrder_totalPrice = new System.Windows.Forms.Label();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel2.SuspendLayout();
@@ -86,16 +89,16 @@
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
-            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.Color.LightSeaGreen;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle9.BackColor = System.Drawing.Color.White;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle9;
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle10.BackColor = System.Drawing.Color.LightSeaGreen;
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle10.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle10.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle10.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle10.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle10;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(13, 71);
             this.dataGridView1.Name = "dataGridView1";
@@ -323,16 +326,16 @@
             // 
             this.dataGridView2.AllowUserToAddRows = false;
             this.dataGridView2.AllowUserToDeleteRows = false;
-            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
-            this.dataGridView2.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle3;
-            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle4.BackColor = System.Drawing.Color.LightSeaGreen;
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle11.BackColor = System.Drawing.Color.White;
+            this.dataGridView2.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle11;
+            dataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle12.BackColor = System.Drawing.Color.LightSeaGreen;
+            dataGridViewCellStyle12.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle12.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle12.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle12.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle12.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle12;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView2.Location = new System.Drawing.Point(20, 71);
             this.dataGridView2.Name = "dataGridView2";
@@ -367,6 +370,7 @@
             this.cashierOrder_receipt.TabIndex = 27;
             this.cashierOrder_receipt.Text = "Receipt";
             this.cashierOrder_receipt.UseVisualStyleBackColor = false;
+            this.cashierOrder_receipt.Click += new System.EventHandler(this.cashierOrder_receipt_Click);
             // 
             // cashierOrder_payOrders
             // 
@@ -444,6 +448,21 @@
             this.cashierOrder_totalPrice.TabIndex = 22;
             this.cashierOrder_totalPrice.Text = "0.00";
             // 
+            // printDocument1
+            // 
+            this.printDocument1.BeginPrint += new System.Drawing.Printing.PrintEventHandler(this.printDocument1_BeginPrint);
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
             // CashierOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -498,5 +517,7 @@
         private System.Windows.Forms.Label cashierOrder_change;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
     }
 }
--- CashierOrder.cs
+++ CashierOrder.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using System.Diagnostics;
+using System.Drawing.Printing;
 
 namespace Invento
 {
@@ -481,6 +482,96 @@ namespace Invento
                     cashierOrder_change.Text = "";
                 }
             }
+        }
+
+        private int rowIndex = 0;
+        private void cashierOrder_receipt_Click(object sender, EventArgs e)
+        {
+            printDocument1.PrintPage += new PrintPageEventHandler(printDocument1_PrintPage);
+            printDocument1.BeginPrint += new PrintEventHandler(printDocument1_BeginPrint);
+
+            printPreviewDialog1.Document = printDocument1;
+            printPreviewDialog1.ShowDialog();
+        }
+
+        private void printDocument1_BeginPrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+            rowIndex = 0;
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            displayTotalPrice();
+
+            float y = 0;
+            int count = 0;
+            int colWidth = 120;
+            int headerMargin = 10;
+            int tableMargin = 20;
+
+            Font font = new Font("Times New Roman", 12);
+            Font bold = new Font("Times New Roman", 12, FontStyle.Bold);
+            Font headerFont = new Font("Times New Roman", 16, FontStyle.Bold);
+            Font labelFont = new Font("Times New Roman", 14);
+
+            float margin = e.MarginBounds.Top;
+
+            StringFormat alignCenter = new StringFormat();
+            alignCenter.Alignment = StringAlignment.Center;
+            alignCenter.LineAlignment = StringAlignment.Center;
+
+            string headerText = "Invento";
+            y = (margin + count * headerFont.GetHeight(e.Graphics) + headerMargin);
+            e.Graphics.DrawString(headerText, headerFont, Brushes.Black, e.MarginBounds.Left
+                + (dataGridView2.ColumnCount / 2) * colWidth, y, alignCenter);
+
+            count++;
+
+            y += tableMargin;
+
+            string[] header = { "CID", "PID", "PName", "Category", "QTY", "OrigPrice", "TotalPrice" };
+
+            for (int q = 0; q < header.Length; q++)
+            {
+                y = margin + count * bold.GetHeight(e.Graphics) + tableMargin;
+                e.Graphics.DrawString(header[1], bold, Brushes.Black, e.MarginBounds.Left + idGen * colWidth, y, alignCenter);
+            }
+            count++;
+
+            float rSpace = e.MarginBounds.Bottom - y;
+
+            while (rowIndex < dataGridView2.Rows.Count) 
+            {
+                DataGridViewRow row = dataGridView2.Rows[rowIndex];
+
+                for (int q = 0; q < dataGridView2.Columns.Count; q++)
+                {
+                    object cellValue = row.Cells[q].Value;
+                    string cell = (cellValue != null) ? cellValue.ToString() : string.Empty;
+
+                    y = margin + count * font.GetHeight(e.Graphics) + tableMargin;
+                    e.Graphics.DrawString(cell, font, Brushes.Black, e.MarginBounds.Left + idGen * colWidth, y, alignCenter);
+                }
+                count++;
+                rowIndex++;
+
+                if (y + font.GetHeight(e.Graphics) > e.MarginBounds.Bottom)
+                {
+                    e.HasMorePages = true;
+                    return;
+                }
+            }
+            int labelMargin = (int)Math.Min(rSpace, 200);
+
+            DateTime today = DateTime.Now;
+
+            float labelX = e.MarginBounds.Right - e.Graphics.MeasureString("----------------------------", labelFont).Width;
+
+            y = e.MarginBounds.Bottom - labelMargin - labelFont.GetHeight(e.Graphics);
+            e.Graphics.DrawString("Total Price: \t$" + totalPrice + "\nAmont: \t$" + cashierOrder_amount.Text.Trim()
+                + "\n\t\t--------\nChange: \t$" + cashierOrder_change.Text.Trim(), labelFont, Brushes.Black, labelX, y);
+
+            labelMargin = (int)Math.Min(rSpace, -40);
         }
     }
 }
