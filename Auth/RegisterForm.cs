52b3e7ec9c93bd65e363e85f6e173253d2d3f1ae
Radostin <Radostin@DESKTOP-5G5P7FJ>
2025-01-13 19:56:05 +0200

    design for the login done, the register almost the labels are laggy, the customers design is done

--- Auth/RegisterForm.cs
+++ Auth/RegisterForm.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
+using System.Drawing.Drawing2D;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
